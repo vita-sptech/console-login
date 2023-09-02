6b8dec16723dc6505819046674b8ca726950d601
lucasgianine <lucas.esilva@sptech.school>
2023-09-02 16:36:47 -0300

    feat: Creating new validations ✨

--- login-do-sistema/src/main/java/SistemaLogin.java
+++ login-do-sistema/src/main/java/SistemaLogin.java
@@ -8,38 +8,78 @@ public class SistemaLogin {
     List<String> listaSenha = new ArrayList<>();
 
     void cadastro(){
-        System.out.println("Cadastro");
-            System.out.println("Digite um email: ");
+        System.out.println("Cadastre-se!");
+        System.out.println("---------");
+
+        do {
+            System.out.println("• Digite um email: ");
             String email = leitor.nextLine();
 
-            System.out.println("digite uma senha: ");
+            if (email.equals("") && email.equals(" ")
+                    || (email.indexOf("@") == -1 || email.indexOf("@") == 0)
+                    || (email.indexOf(".") == -1 || email.indexOf(".") == 0)
+            ) {
+                System.out.println("Email incorreto, tente novamente!");
+            } else {
+                listaEmail.add(email);
+                break;
+            }
+        } while (true);
+
+        do {
+            System.out.println("• Digite uma senha: ");
             String senha = leitor.nextLine();
 
-            listaEmail.add(email);
-            listaSenha.add(senha);
-            System.out.println("Cadastro realizado com sucesso!");
+            if (senha.length() < 8 || senha.length() > 64) {
+                System.out.println("Senha fraca, informe uma senha mais forte!");
+            } else {
+                listaSenha.add(senha);
+                break;
+            }
+        } while (true);
 
+        System.out.println("""
+                    |----------------------------------|
+                    |       CADASTRO REALIZADO!        |
+                    |     Acesse seu email para ter    |
+                    | a chave de acesso da sua empresa.|
+                    |----------------------------------|
+                    """);
+        System.out.println("Redirecionando ao login...");
+        System.out.println("--------------------------------");
         login();
     }
     void login() {
-        System.out.println("Login");
-            System.out.println("Digite  seu email: ");
-            String email = leitor.nextLine();
-            System.out.println("Digite  sua senha: ");
-            String senha = leitor.nextLine();
+        Boolean loginBemSucedido = false;
 
-            boolean loginBemSucedido = false;
+        do {
+            System.out.println("Bem-vindo(a) de volta!");
+            System.out.println("---------");
+
+            System.out.println("• Digite seu email: ");
+            String email = leitor.nextLine();
+
+            System.out.println("• Digite  sua senha: ");
+            String senha = leitor.nextLine();
 
             for (int i = 0; i < listaEmail.size(); i++) {
                 if (listaEmail.get(i).equals(email) && listaSenha.get(i).equals(senha)) {
                     loginBemSucedido = true;
                     break;
                 }
-                }
-            if(loginBemSucedido){
-                System.out.println("Login bem-sucedido!");
-            }else {
-                    System.out.println("Nome de usuário ou senha incorretos. Tente novamente.");
-                }
-            while (!loginBemSucedido) ;
-    }}
+            }
+
+            if (loginBemSucedido) {
+                System.out.println("""
+                        |----------------------------------|
+                        |       LOGIN BEM SUCEDIDO!        |
+                        | Bem-vindo(a) de volta ao sistema |
+                        |           da idleCare            |
+                        |----------------------------------|
+                        """);
+            } else {
+                System.out.println("Nome de usuário e/ou senha incorretos. Tente novamente.");
+            }
+        } while (!loginBemSucedido);
+    }
+}

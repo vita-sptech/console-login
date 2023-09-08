9b167ac5dbf27721500d73eaee6e23a7d45dccae
lucasgianine <lucas.esilva@sptech.school>
2023-09-08 14:09:35 -0300

    feat: Tipo de usuário e Ger. de máquinas

--- login-do-sistema/src/main/java/Autenticacao/SistemaLogin.java
+++ login-do-sistema/src/main/java/Autenticacao/SistemaLogin.java
@@ -8,12 +8,16 @@ import java.util.Scanner;
 
 public class SistemaLogin {
     Scanner leitor = new Scanner(System.in);
+    Scanner leitor2 = new Scanner(System.in);
+
     List<String> listaEmail = new ArrayList<String>();
     List<String> listaSenha = new ArrayList<String>();
     List<String> dateLog = new ArrayList<String>();
 
     DateTimeFormatter formatadorDataHora =
             DateTimeFormatter.ofPattern("dd/MM/yyyy - hh:mm:ss");
+
+    Boolean isAdmin = false;
 
     void cadastro(){
         System.out.println("Cadastre-se!");
@@ -44,6 +48,20 @@ public class SistemaLogin {
                 listaSenha.add(senha);
                 break;
             }
+        } while (true);
+
+        do {
+            System.out.println("• Tipo de usuário:\n1) Admin\n2) Suporte");
+            Integer tipoUsuario = leitor2.nextInt();
+
+                if (tipoUsuario == 1) {
+                    isAdmin = true;
+                    break;
+            } else if (tipoUsuario == 2) {
+                    break;
+                } else {
+                    System.out.println("❌ Tipo de usuário inválido!");
+                }
         } while (true);
 
         System.out.println("""
@@ -91,10 +109,152 @@ public class SistemaLogin {
                         |           da idleCare            |
                         +----------------------------------+
                         """);
+
+                    profile();
             } else {
                 System.out.println("❌ Nome de usuário e/ou senha incorretos. Tente novamente.");
             }
         } while (!loginBemSucedido);
+    }
+
+    void profile() {
+        List<String> listaMaquinasNumDeSerie = new ArrayList();
+        List<String> listaMaquinasApelido = new ArrayList();
+        List<Double> listaMaquinasRAM = new ArrayList();
+        List<Double> listaMaquinasCPU = new ArrayList();
+        List<String> listaMaquinasLocalizacao = new ArrayList();
+
+        do {
+            System.out.println("""
+                    +----------------------------------+
+                     Email: %s
+                     Admin: %b
+                    +----------------------------------+
+                    """);
+            System.out.println("""
+                    | 1) Cadastrar máquina             |
+                    | 2) Remover máquina               |
+                    | 3) Listar máquinas               |
+                    | 4) Deslogar                      |
+                    +----------------------------------+
+                    """);
+            Integer escolha = leitor2.nextInt();
+
+            if (escolha == 1) {
+                System.out.println("• Digite o número de série:");
+                String numeroDeSerie = leitor.nextLine();
+
+                if (numeroDeSerie.equals("")) {
+                    System.out.println("❌ Informe um número de série válido.");
+                } else {
+                    listaMaquinasNumDeSerie.add(numeroDeSerie);
+                }
+
+                System.out.println("• Digite o apelido da máquina:");
+                String apelido = leitor.nextLine();
+
+                if (apelido.equals("")) {
+                    System.out.println("❌ Informe um apelido válido.");
+                } else {
+                    listaMaquinasApelido.add(apelido);
+                }
+
+                System.out.println("• Quantidade de memória RAM:");
+                Double memoriaRAM = leitor2.nextDouble();
+
+                if (memoriaRAM < 0.0) {
+                    System.out.printf("❌ Informe um número válido.");
+                } else {
+                    listaMaquinasRAM.add(memoriaRAM);
+                }
+
+                System.out.printf("• Processador (CPU):");
+                Double processador = leitor2.nextDouble();
+
+                if (processador < 0.0) {
+                    System.out.printf("❌ Informe um número válido.");
+                } else {
+                    listaMaquinasCPU.add(processador);
+                }
+
+                System.out.printf("• Localidade da maquina (atualmente):");
+                String localidade = leitor.nextLine();
+
+                if (localidade.equals("")) {
+                    System.out.printf("❌ Informe uma localização válida.");
+                } else {
+                    listaMaquinasLocalizacao.add(localidade);
+                }
+
+                System.out.println("Cadastro realizado com sucesso!");
+
+            } else if (escolha == 2) {
+                if (listaMaquinasNumDeSerie.size() > 0) {
+                    System.out.println("Informe o ID da máquina que deseja remover");
+                    Integer maquina = leitor2.nextInt();
+
+                    if (maquina <= listaMaquinasApelido.size() && maquina > 0) {
+                        listaMaquinasNumDeSerie.remove(maquina - 1);
+                        listaMaquinasApelido.remove(maquina - 1);
+                        listaMaquinasRAM.remove(maquina - 1);
+                        listaMaquinasCPU.remove(maquina - 1);
+                        listaMaquinasLocalizacao.remove(maquina - 1);
+                    } else {
+                        System.out.println("❌ Não existe nenhuma máquina com esse ID.");
+                    }
+                } else {
+                    System.out.println("❌ Não existe nenhuma máquina cadastrada.");
+                }
+            } else if (escolha == 3) {
+                if (listaMaquinasNumDeSerie.size() > 0) {
+                    System.out.println("Listando todas as máquinas:");
+
+                    for (int i = 0; i < listaMaquinasNumDeSerie.size(); i++) {
+                        System.out.println("+----");
+                        System.out.println(
+                                String.format("""
+                                %d - Máquina: %s
+                                """, i + 1, listaMaquinasApelido.get(i))
+                        );
+                    }
+
+                    System.out.println("""
+                            +----------------------------------+
+                            Deseja visualizar detalhes de alguma máquina?
+                            Informe o ID da máquina. (Digite 0 caso não queira)
+                            """);
+                    Integer maquina = leitor2.nextInt();
+
+                    if (maquina <= listaMaquinasApelido.size() && maquina > 0) {
+                        System.out.println(
+                                String.format("""
+                                Máquina ID: %d
+                                Número de Série: %s
+                                Apelido: %s
+                                Memória RAM: %f
+                                CPU: %f
+                                Localização: %s
+                                """, maquina,
+                                listaMaquinasNumDeSerie.get(maquina - 1),
+                                listaMaquinasApelido.get(maquina - 1),
+                                listaMaquinasRAM.get(maquina - 1),
+                                listaMaquinasCPU.get(maquina - 1),
+                                listaMaquinasLocalizacao.get(maquina - 1)
+                                )
+                        );
+                    } else {
+                        System.out.println("❌ Não existe nenhuma máquina com esse ID.");
+                    }
+                } else {
+                    System.out.println("❌ Não existe nenhuma máquina cadastrada.");
+                }
+            } else if (escolha == 4) {
+                System.out.println("Deslogando...");
+                break;
+            } else {
+                System.out.println("❌ Informe uma opção válida.");
+            }
+        } while (true);
     }
 
     private static String maskPassword(String senha) {
